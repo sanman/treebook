e8fbf1aa29c6b10fc9be12234e2f95be3c48b5c3
sanman <learnsanjeev@gmail.com>
2013-05-03 20:37:31 +0530

    added frontend module

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'ba00e60da1f13345603c4ab1a7447aec7e5461d36da56a40bafa8a5b49245f4791b974b0e5cac9de957402383d34eeb649fb024a515a0655b2b69433a7f2f7e2'
